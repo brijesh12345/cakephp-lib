tekniq/cakephp

when using App::path but expecting 1 result - use the first

lib/Cake/Console/Command/Task/PluginTask.php
@@ -41,7 +41,7 @@ class PluginTask extends Shell {
  * @return void
  */
 	function initialize() {
-		$this->path = App::path('plugins');
+		$this->path = current(App::path('plugins'));
 	}
 
 /**

brijesh12345/cakephp-lib

Console/Command/Task/PluginTask.php
@@ -41,7 +41,7 @@ class PluginTask extends Shell {
  * @return void
  */
 	function initialize() {
-		$this->path =  APP . 'Plugin' . DS;
+		$this->path = current(App::path('Plugin'));
 	}
 
 /**